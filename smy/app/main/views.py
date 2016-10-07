c4bf9d15f0d7f9eb253bbfb66ac87e64ec0c0cd6
yao sun <yao sun>
2016-10-07 12:05:33 +0800

    second commit

---
+++ smy/app/main/views.py
@@ -0,0 +1,163 @@
+# coding:utf-8
+from . import main
+from .. import db
+from app.models import User, Post, Category, Tie
+from .forms import LoginForm, PostForm, EditForm
+from flask import render_template, redirect, url_for, flash, request
+from flask.ext.login import login_user, logout_user, current_user, login_required
+
+
+@main.route('/')
+def index():
+    page = request.args.get('page', 1, type=int)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=8, error_out=False)
+    posts = pagination.items
+    # tie = []
+    # for i in posts:
+    # ties.append(Tie.query.filter_by(post_id=i.id).all())
+    ties = Tie.query.all()
+    cates = Category.query.all()
+    for ca in cates:
+        ca.count = Tie.query.filter(Tie.cate_id == ca.id).count()
+    db.session.commit()
+    return render_template('index.html', posts=posts, pagination=pagination, ties=ties, cates=cates)
+
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if form.validate_on_submit():
+        # if request.method == "POST" and form.validate():
+        # if request.method == "POST":
+        flash("zhengque")
+        u = User.query.filter_by(username=form.username.data).first()
+        if u is not None and u.verify_password(form.password.data):
+            login_user(u, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Please check your email or password')
+        # return redirect(request.args.get('next') or url_for('main.index'))
+    return render_template('login.html', form=form)
+
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    # 竟然不需要加current_user
+    flash("Logout Success")
+    return redirect(url_for('main.index'))
+
+
+@main.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@main.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+@main.route('/writepost', methods=['GET', 'POST'])
+@login_required
+def writepost():
+    form = PostForm(request.form)
+    cate = Category.query.all()
+    flash('成功1')
+    if form.validate_on_submit():
+        flash('成功')
+        # fo = form.category.data.split(',')
+        # for i in fo[0:-1]:
+        #     c = Category.query.filter_by(tag=i).first()
+        #     if c is not None:
+        #         pos = Post(title=form.title.data, body=form.body.data, category_id=c.id)
+        #         db.session.add(pos)
+        # db.session.commit()
+        # return redirect(url_for('main.index'))
+        pos = Post(title=form.title.data, body=form.body.data)
+        db.session.add(pos)
+        fo = form.category.data.split(',')
+        for i in fo[0:-1]:
+            c = Category.query.filter_by(tag=i).first()
+            if c is not None:
+                tie = Tie(post_id=pos.id, cate_id=c.id)
+                db.session.add(tie)
+        # for ca in cate:
+        #     ca.count = Tie.query.filter(Tie.cate_id == ca.id).count()
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    return render_template('writepost.html', form=form, the_category=cate)
+
+
+@main.route('/post/<id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    ties = Tie.query.filter_by(post_id=id).all()
+    return render_template('posts.html', post=post, ties=ties)
+
+
+@main.route('/category/<tag>')
+def category(tag):
+    ties = Tie.query.all()
+    category = Category.query.all()
+    page = request.args.get('page', 1, type=int)
+    # session.query(User, Address).filter(User.id == Address.user_id)
+    # 三表连接查询
+    pos = Post.query.join(Tie).join(Category).filter(Category.tag == tag)
+    pagination = pos.order_by(Post.timestamp.desc()).paginate(
+        page, per_page=8, error_out=False)
+    posts = pagination.items
+    cates = Category.query.all()
+    return render_template('index.html', posts=posts, pagination=pagination, ties=ties, cates=cates)
+
+
+@main.route('/editpost/<id>', methods=['GET', 'POST'])
+def edit(id):
+    form = EditForm(request.form)
+    cate = Category.query.all()
+    po = Post.query.filter(Post.id == id).first()
+    if form.validate_on_submit():
+        # fo = form.category.data.split(',')
+        # for i in fo[0:-1]:
+        #     c = Category.query.filter_by(tag=i).first()
+        #     if c is not None:
+        #         pos = Post(title=form.title.data, body=form.body.data, category_id=c.id)
+        #         db.session.add(pos)
+        # db.session.commit()
+        # return redirect(url_for('main.index'))
+        # pos = Post(title=form.title.data, body=form.body.data)
+        # db.session.add(pos)
+        Tie.query.filter(Tie.post_id == id).delete()
+        Post.query.filter(Post.id == id).update({'title' : form.title.data,'body' : form.body.data})
+        fo = form.category.data.split(',')
+        for i in fo[0:-1]:
+            c = Category.query.filter_by(tag=i).first()
+            if c is not None:
+                tie = Tie(post_id=id, cate_id=c.id)
+                db.session.add(tie)
+                # Tie.query.filter(Tie.post_id == id).update({'cate_id' : c.id})
+        # for ca in cate:
+        #     ca.count = Tie.query.filter(Tie.cate_id == ca.id).count()
+        db.session.commit()
+        return redirect(url_for('main.index'))
+
+    form.title.data = po.title
+    form.body.data = po.body
+    poss = Category.query.join(Tie).join(Post).filter(Post.id == id).all()
+    if poss is not None:
+        # form.category.data = poss[0].tag
+        for pos in poss:
+            form.category.data = str(form.category.data) + ',' + str(pos.tag) 
+        # pos.tag返回的是unicode值
+    return render_template('editpost.html', form=form, the_category=cate)
+
+
+@main.route('/deletepost/<id>', methods=['GET', 'POST'])
+def delete(id):
+    post = Post.query.filter(Post.id == id).first()
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('main.index'))
+
+
---
+++ smy/app/models.py
@@ -0,0 +1,80 @@
+# coding: utf-8
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import login_manager
+from flask.ext.login import UserMixin
+from flask import current_app, request
+from datetime import datetime
+import hashlib
+from markdown import markdown
+import bleach
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    password_hash = db.Column(db.String(128), unique=True)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text)
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    body_html = db.Column(db.Text)
+    # category_id = db.Column(db.Integer, db.ForeignKey('categorys.id'))
+    tie_post = db.relationship(
+        "Tie", backref="pos", lazy='dynamic', passive_deletes=True)
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        attrs = {
+            '*': ['class'],
+            'a': ['href', 'rel'],
+            'img': ['src', 'alt'],
+        }
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, attributes=attrs, strip=True))
+db.event.listen(Post.body, 'set', Post.on_changed_body)
+
+
+
+class Category(db.Model):
+    __tablename__ = "categorys"
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(64), unique=True)
+    count = db.Column(db.Integer)
+    # posts = db.relationship("Post", backref="category", lazy='dynamic')
+    tie_cate = db.relationship("Tie", backref="category", lazy='dynamic')
+
+# db.event.listen(Category.count, 'set', Category.leng())
+class Tie(db.Model):
+    __tablename__ = "ties"
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id', ondelete='CASCADE'))
+    cate_id = db.Column(db.Integer, db.ForeignKey('categorys.id'))
+
+    
+
